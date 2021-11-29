7f5a0b0bca0bf07b5df3879065f4bd17dd3e34b9
Matt Riese <riese.matt@gmail.com>
2021-11-29 12:40:29 -0800

    added auth

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+console.log('it this thing on??? : provider', provider);
